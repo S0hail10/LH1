40cfae8769bf7b5aad373a765f30620ff8f52a49
sohail.p1 <sohail.p1@schule.bremen.de>
2022-10-05 11:32:21 +0200

    Verzweigung

--- src/AB/Maexchen.java
+++ src/AB/Maexchen.java
@@ -4,13 +4,16 @@ import opp.IO;
 
 import java.io.IOException;
 
-public class Mäxchen {
+public class Maexchen {
     public static void main(String[] args) throws IOException {
         System.out.println("Bitte die erste Augenzahl eingeben.");
-        double ersteAZ = IO.readDouble();
+        int ersteAZ = IO.readInteger();
         System.out.println("Bitte die zweite Augenzahl eingeben.");
-        double zweiteAZ = IO.readDouble();
+        int zweiteAZ = IO.readInteger();
 
-         if (ersteAZ = 1 && zweiteAZ =1 )
+         if ((ersteAZ == 1) && (zweiteAZ == 2) || (ersteAZ == 2 && zweiteAZ == 1) ) {
+             System.out.println("1000 Punkte");
+         }
+
     }
 }
